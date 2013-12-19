f802900d905580a13ecd910f1d3186656cdd34d2
Julian Espinel <julianespinel@gmail.com>
2013-12-19 18:26:08 -0500

    Add support for CRUD operations on the Permission resource. Resolves #11.

--- app/repository/PermissionRepository.js
+++ app/repository/PermissionRepository.js
@@ -2,20 +2,43 @@
  * This module represents a repository for the table permissions
  * @param {Sequelize} permissionModel the model created by sequelize
  */
-module.exports = function(permissionModel){
+module.exports = function(permissionModel) {
 
 	/**
-	 * finds all results of the permissions table according to the params offset and limit
+	 * Finds all results of the permissions table according to the params offset and limit
 	 */
-	permissionModel.getAllPermissions = function(options, success, error){
+	permissionModel.getAllPermissions = function(options, success, error) {
 		permissionModel.findAll({offset: options.offset, limit: options.limit}).success(success).error(error);
 	}
+
+    /**
+     * Update a permission given its name.
+     */
+    permissionModel.getPermissionByName = function(permissionName, updatedPermission, success, error) {
+        // update(updated entity, where clause)
+        permissionModel.find({ where: { name: permissionName } }).success(success).error(error);
+    }
+
+    /**
+     * Update a permission given its name.
+     */
+    permissionModel.updatePermissionByName = function(permissionName, updatedPermission, success, error) {
+        // update(updated entity, where clause)
+        permissionModel.update(updatedPermission, { name: permissionName }).success(success).error(error);
+    }
+
+    /**
+     * Delete a permission given its name.
+     */
+    permissionModel.deletePermissionByName = function(permissionName, success, error) {
+        // update(updated entity, where clause)
+        permissionModel.delete({ name: permissionName }).success(success).error(error);
+    }
 
     /**
      * Gets the permissions of the given role.
      */
     permissionModel.findPermissionsByRole = function(role, success, error) {
-
         permissionModel.findAll({ where: { roleId: role.id } }).success(success).error(error);
     }
 
---
+++ test/business/PermissionBusinessTest.js
@@ -0,0 +1,486 @@
+var should = require('should');
+var rewire = require("rewire");
+var logger = require("../../config/logger");
+
+var constants = require("../../config/constants");
+
+describe('PermissionBusiness', function () {
+
+    describe('getAllPermissions method', function () {
+
+        it('should return 200 OK', function (done) {
+
+            //attention: it's rewire not require :)
+            var permissionBusiness = rewire("../../app/business/PermissionBusiness");
+
+            var expectedResult = {
+                foo: 1
+            }
+
+            var expectedStatus = 200;
+
+            var request = {
+                param: function(name){
+                    //mocks offset and limit
+                    return 10;
+                }
+            };
+
+            var response = {
+                status: function(status) {
+                    status.should.equal(expectedStatus);
+                    return this;
+                },
+                json: function(result) {
+
+                    result.should.equal(expectedResult);
+                    done();
+                }
+            };
+
+            /**
+             * Mock all needed methods and properties to the element we try to test
+             */
+            var mocks = function() {
+
+                //mock getRepository function (i.e. the repository). This way, we do not need the database connection
+                permissionBusiness.__set__("repositoryFactory", {
+                    getPermissionRepository: function(req) {
+                        return {
+                            getAllPermissions: function(options, success, error) {
+                                success(expectedResult);
+                            }
+                        }
+                    }
+                });
+            }
+
+            //set up mocks
+            mocks();
+
+            permissionBusiness.getAllPermissions(request, response);
+        });
+
+        it('should return 500 Error', function (done) {
+
+            //attention: it's rewire not require :)
+            var permissionBusiness = rewire("../../app/business/PermissionBusiness");
+
+            var expectedResult = {
+                'error': 'error message'
+            }
+
+            var expectedStatus = 500;
+
+            var request = {
+                param: function(name){
+                    //mocks offset and limit
+                    return 10;
+                }
+            };
+
+            var response = {
+                status: function(status) {
+                    status.should.equal(expectedStatus);
+                    return this;
+                },
+                json: function(result) {
+
+                    result.should.equal(expectedResult);
+                    done();
+                }
+            };
+
+            /**
+             * Mock all needed methods and properties to the element we try to test
+             */
+            var mocks = function() {
+
+                //mock getRepository function (i.e. the repository). This way, we do not need the database connection
+                permissionBusiness.__set__("repositoryFactory", {
+                    getPermissionRepository: function(req) {
+                        return {
+                            getAllPermissions: function(options, success, error) {
+                                error(expectedResult);
+                            }
+                        }
+                    }
+                });
+            }
+
+            //set up mocks
+            mocks();
+
+            permissionBusiness.getAllPermissions(request, response);
+        });
+    });
+
+    describe('getPermissionByName method', function () {
+
+        it('should return 200 OK', function (done) {
+
+            //attention: it's rewire not require :)
+            var permissionBusiness = rewire("../../app/business/PermissionBusiness");
+
+            var expectedResult = {
+                foo: 1
+            }
+
+            var expectedStatus = 200;
+
+            var permissionName = 'Admin';
+            var request = {
+
+                params: {
+                    permissionName: permissionName
+                }
+            };
+
+            var response = {
+                status: function(status) {
+                    status.should.equal(expectedStatus);
+                    return this;
+                },
+                json: function(result) {
+
+                    result.should.equal(expectedResult);
+                    done();
+                }
+            };
+
+            // Verify the request if formed properly.
+            request.params.permissionName.should.equal(permissionName);
+
+            /**
+             * Mock all needed methods and properties to the element we try to test
+             */
+            var mocks = function() {
+
+                //mock getRepository function (i.e. the repository). This way, we do not need the database connection
+                permissionBusiness.__set__("repositoryFactory", {
+                    getPermissionRepository: function(req) {
+                        return {
+                            getPermissionByName: function(permissionName, success, error) {
+                                success(expectedResult);
+                            }
+                        }
+                    }
+                });
+            }
+
+            //set up mocks
+            mocks();
+
+            permissionBusiness.getPermissionByName(request, response);
+        });
+
+        it('should return 500 Error', function (done) {
+
+            //attention: it's rewire not require :)
+            var permissionBusiness = rewire("../../app/business/PermissionBusiness");
+
+            var expectedResult = {
+                foo: 1
+            }
+
+            var expectedStatus = 500;
+
+            var permissionName = 'Admin';
+            var request = {
+
+                params: {
+                    permissionName: permissionName
+                }
+            };
+
+            var response = {
+                status: function(status) {
+                    status.should.equal(expectedStatus);
+                    return this;
+                },
+                json: function(result) {
+
+                    result.should.equal(expectedResult);
+                    done();
+                }
+            };
+
+            // Verify the request if formed properly.
+            request.params.permissionName.should.equal(permissionName);
+
+            /**
+             * Mock all needed methods and properties to the element we try to test
+             */
+            var mocks = function() {
+
+                //mock getRepository function (i.e. the repository). This way, we do not need the database connection
+                permissionBusiness.__set__("repositoryFactory", {
+                    getPermissionRepository: function(req) {
+                        return {
+                            getPermissionByName: function(permissionName, success, error) {
+                                error(expectedResult);
+                            }
+                        }
+                    }
+                });
+            }
+
+            //set up mocks
+            mocks();
+
+            permissionBusiness.getPermissionByName(request, response);
+        });
+    });
+
+    describe('updatePermissionByName method', function () {
+
+        it('should return 200 OK', function (done) {
+
+            //attention: it's rewire not require :)
+            var permissionBusiness = rewire("../../app/business/PermissionBusiness");
+
+            var expectedResult = {
+                'name': 'root',
+                'httpVerb': 'get',
+                'uri': '/api/permissions'
+            }
+
+            var expectedStatus = 200;
+
+            var permissionName = 'admin';
+            var request = {
+
+                params: {
+                    permissionName: permissionName
+                },
+
+                body: {
+                    'name': 'root',
+                    'httpVerb': 'get',
+                    'uri': '/api/permissions'
+                }
+            };
+
+            var response = {
+                status: function(status) {
+                    status.should.equal(expectedStatus);
+                    return this;
+                },
+                json: function(result) {
+
+                    result.should.equal(expectedResult);
+                    done();
+                }
+            };
+
+            // Verify the request if formed properly.
+            request.params.permissionName.should.equal(permissionName);
+            request.body.should.not.be.empty;
+
+            /**
+             * Mock all needed methods and properties to the element we try to test
+             */
+            var mocks = function() {
+
+                //mock getRepository function (i.e. the repository). This way, we do not need the database connection
+                permissionBusiness.__set__("repositoryFactory", {
+                    getPermissionRepository: function(req) {
+                        return {
+                            updatePermissionByName: function(permissionName, updatedPermission, success, error) {
+                                success(expectedResult);
+                            }
+                        }
+                    }
+                });
+            }
+
+            //set up mocks
+            mocks();
+
+            permissionBusiness.updatePermissionByName(request, response);
+        });
+
+        it('should return 500 Error', function (done) {
+
+            //attention: it's rewire not require :)
+            var permissionBusiness = rewire("../../app/business/PermissionBusiness");
+
+            var expectedResult = {
+                'error': 'error message'
+            }
+
+            var expectedStatus = 500;
+
+            var permissionName = 'admin';
+            var request = {
+
+                params: {
+                    permissionName: permissionName
+                },
+
+                body: {
+                    'name': 'root',
+                    'httpVerb': 'get',
+                    'uri': '/api/permissions'
+                }
+            };
+
+            var response = {
+                status: function(status) {
+                    status.should.equal(expectedStatus);
+                    return this;
+                },
+                json: function(result) {
+
+                    result.should.equal(expectedResult);
+                    done();
+                }
+            };
+
+            // Verify the request if formed properly.
+            request.params.permissionName.should.equal(permissionName);
+            request.body.should.not.be.empty;
+
+            /**
+             * Mock all needed methods and properties to the element we try to test
+             */
+            var mocks = function() {
+
+                //mock getRepository function (i.e. the repository). This way, we do not need the database connection
+                permissionBusiness.__set__("repositoryFactory", {
+                    getPermissionRepository: function(req) {
+                        return {
+                            updatePermissionByName: function(permissionName, updatedPermission, success, error) {
+                                error(expectedResult);
+                            }
+                        }
+                    }
+                });
+            }
+
+            //set up mocks
+            mocks();
+
+            permissionBusiness.updatePermissionByName(request, response);
+        });
+    });
+
+    describe('deletePermissionByName method', function () {
+
+        it('should return 200 OK', function (done) {
+
+            //attention: it's rewire not require :)
+            var permissionBusiness = rewire("../../app/business/PermissionBusiness");
+
+            var expectedResult = {
+                'name': 'root',
+                'httpVerb': 'get',
+                'uri': '/api/permissions'
+            }
+
+            var expectedStatus = 200;
+
+            var permissionName = 'admin';
+            var request = {
+
+                params: {
+                    permissionName: permissionName
+                }
+            };
+
+            var response = {
+                status: function(status) {
+                    status.should.equal(expectedStatus);
+                    return this;
+                },
+                json: function(result) {
+
+                    result.should.equal(expectedResult);
+                    done();
+                }
+            };
+
+            // Verify the request if formed properly.
+            request.params.permissionName.should.equal(permissionName);
+
+            /**
+             * Mock all needed methods and properties to the element we try to test
+             */
+            var mocks = function() {
+
+                //mock getRepository function (i.e. the repository). This way, we do not need the database connection
+                permissionBusiness.__set__("repositoryFactory", {
+                    getPermissionRepository: function(req) {
+                        return {
+                            deletePermissionByName: function(permissionName, success, error) {
+                                success(expectedResult);
+                            }
+                        }
+                    }
+                });
+            }
+
+            //set up mocks
+            mocks();
+
+           permissionBusiness.deletePermissionByName(request, response);
+        });
+
+        it('should return 500 Error', function (done) {
+
+            //attention: it's rewire not require :)
+            var permissionBusiness = rewire("../../app/business/PermissionBusiness");
+
+            var expectedResult = {
+                'error': 'error message'
+            }
+
+            var expectedStatus = 500;
+
+            var permissionName = 'admin';
+            var request = {
+
+                params: {
+                    permissionName: permissionName
+                }
+            };
+
+            var response = {
+                status: function(status) {
+                    status.should.equal(expectedStatus);
+                    return this;
+                },
+                json: function(result) {
+
+                    result.should.equal(expectedResult);
+                    done();
+                }
+            };
+
+            // Verify the request if formed properly.
+            request.params.permissionName.should.equal(permissionName);
+
+            /**
+             * Mock all needed methods and properties to the element we try to test
+             */
+            var mocks = function() {
+
+                //mock getRepository function (i.e. the repository). This way, we do not need the database connection
+                permissionBusiness.__set__("repositoryFactory", {
+                    getPermissionRepository: function(req) {
+                        return {
+                            deletePermissionByName: function(permissionName, success, error) {
+                                error(expectedResult);
+                            }
+                        }
+                    }
+                });
+            }
+
+            //set up mocks
+            mocks();
+
+           permissionBusiness.deletePermissionByName(request, response);
+        });
+    });
+});
